5a247ffc85fb7a85689de391d38eeb28b3f1256f
hsu <wellshsu1004@gmail.com>
2016-09-11 17:47:47 +0800

    state framework

---
+++ Client/Framework/Assets/Scripts/Air2000/Framework/Modular/Extensions/State/StateContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Air2000.Modular
+{
+    public class StateContext : Context
+    {
+        private string m_StateName;//该状态的名称
+        protected StateMachineContext mStateMachine;//所属的状态机实例
+        public string pStateName
+        {
+            get { return m_StateName; }
+            set { m_StateName = value; }
+        }
+        public StateMachineContext pStateMachine
+        {
+            get { return mStateMachine; }
+            set { mStateMachine = value; }
+        }
+        public StateContext(string stateName) { m_StateName = stateName; }
+        public virtual void OnCreated() { }
+        public virtual void Begin() { }
+        public virtual void Update() { }
+        public virtual void End() { }
+        public virtual void OnDestroy() { }
+    }
+}
---
+++ Client/Framework/Assets/Scripts/Air2000/Framework/Modular/Extensions/State/StateMachineContext.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Air2000.Modular
+{
+    public class StateMachineContext : Context
+    {
+        protected Dictionary<string, StateContext> m_States;
+        protected StateContext m_LastState;
+        protected StateContext m_CurrentState;
+        protected StateContext m_NextState;
+
+        public StateMachineContext() : base()
+        {
+            m_States = new Dictionary<string, StateContext>();
+            m_CurrentState = null;
+            m_NextState = null;
+        }
+
+        public virtual void Update()
+        {
+            if (m_CurrentState != null)
+            {
+                m_CurrentState.Update();
+            }
+            if (m_NextState != null)
+            {
+                if (m_CurrentState != null)
+                {
+                    m_CurrentState.End();
+                }
+                m_CurrentState = m_NextState;
+                m_NextState = null;
+                m_CurrentState.Begin();
+            }
+        }
+
+        /// <summary>
+        /// get state with statename
+        /// </summary>
+        /// <param name="stateName"></param>
+        /// <returns></returns>
+        public StateContext GetState(string stateName)
+        {
+            StateContext state = null;
+            if (m_States.TryGetValue(stateName, out state))
+            {
+                return state;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// register a new state to state machine
+        /// </summary>
+        /// <param name="state"></param>
+        /// <returns></returns>
+        public bool RegisterState(StateContext state)
+        {
+            if (state == null)
+            {
+                return false;
+            }
+            string stateName = state.pStateName;
+            if (m_States.ContainsKey(stateName) == true)
+            {
+                return false;
+            }
+            state.pStateMachine = this;
+            m_States.Add(stateName, state);
+            return true;
+        }
+
+        /// <summary>
+        /// unregister state with name
+        /// </summary>
+        /// <param name="stateName"></param>
+        /// <returns></returns>
+        public bool UnRegisterState(string stateName)
+        {
+            StateContext state = null;
+            if (m_States.TryGetValue(stateName, out state) == false)
+            {
+                return false;
+            }
+            state.pStateMachine = null;
+            m_States.Remove(stateName);
+            return true;
+        }
+
+        /// <summary>
+        /// change state
+        /// </summary>
+        /// <param name="stateName"></param>
+        /// <returns></returns>
+        public virtual bool SetNextState(string stateName)
+        {
+            if (string.IsNullOrEmpty(stateName))
+            {
+                return false;
+            }
+            StateContext state = GetState(stateName);
+            if (state == null)
+            {
+                return false;
+            }
+            m_LastState = m_CurrentState;
+            if (state == m_NextState)
+            {
+                return false;
+            }
+            m_NextState = state;
+            return true;
+        }
+
+        /// <summary>
+        /// get state that will be executed in next frame
+        /// </summary>
+        /// <returns></returns>
+        public StateContext GetNextState()
+        {
+            return m_NextState;
+        }
+
+        /// <summary>
+        /// get current executing state
+        /// </summary>
+        /// <returns></returns>
+        public StateContext GetCurrentState()
+        {
+            return m_CurrentState;
+        }
+
+        /// <summary>
+        /// get last executed state
+        /// </summary>
+        /// <returns></returns>
+        public StateContext GetLastState()
+        {
+            return m_LastState;
+        }
+    }
+}
